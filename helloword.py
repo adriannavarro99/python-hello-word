fc8ddf4dd1376e8d36a9fab304b1f76bd8693ad9
adriannavarro99 <adriannavarro92@gmail.com>
2021-09-20 20:17:09 -0600

    fist commit

---
+++ helloword.py
@@ -0,0 +1,18 @@
+print( "Hello World" )
+
+name = "Adrian"
+print( "Hello", name )	
+print( "Hello " + name  )	
+
+
+name = 42
+print("Hello" , 42, "!" )	
+print( "Hello " + str(42) )	
+
+
+food1 = "pizza"
+food2 = "hamburguer"
+
+
+print( "I love to eat {} and {}.".format(food1,food2) ) 
+print( f"I love to eat {food1} and {food2}." ) 
